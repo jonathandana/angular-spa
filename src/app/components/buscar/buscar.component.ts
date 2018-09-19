7f1932b71421ad7b8eb6741f69d2d19c38ab9418
Jonathan Dana <jonathandana27@gmail.com>
2018-09-20 01:17:59 +0300

    New Search componetn

---
+++ src/app/components/buscar/buscar.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import {Heroe, HeroesService} from '../../service/heroes.service';
+import {ActivatedRoute} from '@angular/router';
+
+
+
+
+@Component({
+  selector: 'app-buscar',
+  templateUrl: './buscar.component.html',
+  styles: []
+})
+export class BuscarComponent implements OnInit {
+  heroes: Heroe[] = [];
+  buscador: string;
+
+  constructor(private _heroesServices: HeroesService, private activatedRoute: ActivatedRoute ) {
+
+  }
+
+  ngOnInit() {
+      this.activatedRoute.params.subscribe(params => {
+          this.buscador = params['termino'];
+          this.heroes = this._heroesServices.buscarHeroes(params['termino']);
+      });
+  }
+
+}
